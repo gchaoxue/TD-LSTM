61835445b3e2b2a21f09afd06396c869c8943593
gchaoxue <gchaoxue@gmail.com>
2016-11-26 03:49:54 +0800

    Update lstm_demo.py

--- lstm_demo.py
+++ lstm_demo.py
@@ -1,50 +1,84 @@
 import tensorflow as tf 
 import numpy as np 
 
-lstm_size = 6
-num_class = 10
-input_x = np.array([ [[0.2, 0.4, 0.7],[0, 0, 0]], 
-					 [[0.5, 0.6, 0.2],[0.7, 0.8, 0.2]], 
-					 [[0.9, 1.0, 0.2],[0.2, 0, 0]],
-					 [[1.3, 1.4, 0.2],[1.5, 1.6, 0.2]] ])
+from data import Data
 
-input_x = np.ndarray(shape=np.shape(input_x), buffer=input_x)
-# print(input_x)
+data = Data()
+# data.set_as_test()
+data.get_data()
 
-x = tf.placeholder(tf.float32, [None,2,3])
-used = tf.sign(tf.reduce_max(tf.abs(x), reduction_indices=2))
-length = tf.reduce_sum(used, reduction_indices=1)
-length = tf.cast(length, tf.int32)
+VECTOR_DIM = data.vector_dim
+LSTM_SIZE = VECTOR_DIM
+MAX_TIME_STEP = 50
+NUM_CLASS = 3
+LEARNING_RATE = 0.3
 
-w = tf.Variable(tf.random_normal([lstm_size, num_class]))
-b = tf.Variable(tf.zeros([num_class]))
+def mask_output(output, seq_length):
+	# masking the output vector
+	batch_size = tf.shape(output)[0]
+	max_length = tf.shape(output)[1]
+	out_size = int( output.get_shape()[2] )
+	index = tf.range(0, batch_size) * max_length + (seq_length - 1)
+	flat = tf.reshape(output, [-1, out_size])
+	return tf.gather(flat, index)
 
-lstm = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
-output, state = tf.nn.dynamic_rnn(lstm,
-								  x, 
-								  dtype=tf.float32,
-								  sequence_length=length)
+left_input = tf.placeholder(tf.float32, [None,None,VECTOR_DIM])
+left_seq_length = tf.placeholder(tf.int32, [None])
 
-batch_size = tf.shape(output)[0]
-max_length = tf.shape(output)[1]
-out_size = int( output.get_shape()[2] )
-index = tf.range(0, batch_size) * max_length + (length - 1)
-flat = tf.reshape(output, [-1, out_size])
-last = tf.gather(flat, index)
+right_input = tf.placeholder(tf.float32, [None,None,VECTOR_DIM])
+right_seq_length = tf.placeholder(tf.int32, [None])
 
-# output = tf.transpose(output, [1,0,2])
+y_ = tf.placeholder(tf.float32, [None, NUM_CLASS])
 
-# y = tf.nn.softmax(tf.matmul(output, w) + b)
+weights = tf.Variable(tf.random_normal([LSTM_SIZE*2, NUM_CLASS]))
+biases = tf.Variable(tf.zeros([NUM_CLASS]))
+
+right_weights = tf.Variable(tf.random_normal([LSTM_SIZE, NUM_CLASS]))
+right_biases = tf.Variable(tf.zeros([NUM_CLASS]))
+
+with tf.variable_scope('left_lstm'):
+	left_lstm = tf.nn.rnn_cell.BasicLSTMCell(LSTM_SIZE)
+	left_outputs, left_states = tf.nn.dynamic_rnn(left_lstm,
+												  left_input,
+												  dtype=tf.float32,
+												  sequence_length=left_seq_length)
+
+with tf.variable_scope('right_lstm'):
+	right_lstm = tf.nn.rnn_cell.BasicLSTMCell(LSTM_SIZE)
+	right_outputs, right_states = tf.nn.dynamic_rnn(right_lstm,
+										  			right_input,
+										  			dtype=tf.float32,
+										  			sequence_length=right_seq_length)
+
+left_outputs = mask_output(left_outputs, left_seq_length)
+right_outputs = mask_output(right_outputs, right_seq_length)
+
+comb_outputs = tf.concat(1, [left_outputs, right_outputs])
+
+y = tf.nn.softmax(tf.matmul(comb_outputs, weights) + biases)
+
+loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
+
+true_count = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+acc = tf.reduce_mean(tf.cast(true_count, tf.float32))
 
 init = tf.initialize_all_variables()
 
 with tf.Session() as sess:
 	sess.run(init)
-	las = sess.run(last, feed_dict={x: input_x})
-	out = sess.run(output, feed_dict={x: input_x})
-	print(out)
-	print('++++++++++++++++++++++++++++')
-	print(np.shape(las))
-	print(las)
+	for _ in range(1000):
+		data_batch = data.get_next_batch(data.train, 200)
+		batch_feed_dict = {
+			left_input: data_batch['left_input'],
+			right_input: data_batch['right_input'],
+			left_seq_length: data_batch['left_seq_length'],
+			right_seq_length: data_batch['right_seq_length'],
+			y_: data_batch['labels']
+		}
+		sess.run(train_step, feed_dict=batch_feed_dict)
+		acc_val = sess.run(acc, feed_dict=batch_feed_dict)
+		print(acc_val)
 
 	# print(output)
